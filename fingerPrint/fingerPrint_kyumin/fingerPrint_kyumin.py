e864fa09640d5bfcec8429d5bed2f07366c0c416
kyumin1227 <kyumin12271227@gmail.com>
2024-04-05 16:58:04 +0900

    Feat: UI 디자인 완성 및 페이지 변경 기능 추가

--- fingerPrint/fingerPrint_kyumin/fingerPrint_kyumin.py
+++ fingerPrint/fingerPrint_kyumin/fingerPrint_kyumin.py
@@ -24,21 +24,25 @@ class Ui_MainWindow(object):
         self.fingerprint_button.setStyleSheet("background-color: rgb(78, 78, 78);\n"
 "color: rgb(227, 227, 227); border: 0px;")
         self.fingerprint_button.setObjectName("fingerprint_button")
+        self.fingerprint_button.clicked.connect(lambda: self.changePage(0))
         self.outing_button = QtWidgets.QPushButton(self.centralwidget)
         self.outing_button.setGeometry(QtCore.QRect(40, 140, 120, 95))
         self.outing_button.setStyleSheet("background-color: rgb(78, 78, 78);\n"
 "color: rgb(227, 227, 227); border: 0px;")
         self.outing_button.setObjectName("outing_button")
+        self.outing_button.clicked.connect(lambda: self.changePage(1))
         self.new_fingerprint_button = QtWidgets.QPushButton(self.centralwidget)
         self.new_fingerprint_button.setGeometry(QtCore.QRect(40, 240, 120, 95))
         self.new_fingerprint_button.setStyleSheet("background-color: rgb(78, 78, 78);\n"
 "color: rgb(227, 227, 227); border: 0px;")
         self.new_fingerprint_button.setObjectName("new_fingerprint_button")
+        self.new_fingerprint_button.clicked.connect(lambda: self.changePage(2))
         self.delete_fingerprint_button = QtWidgets.QPushButton(self.centralwidget)
         self.delete_fingerprint_button.setGeometry(QtCore.QRect(40, 340, 120, 95))
         self.delete_fingerprint_button.setStyleSheet("background-color: rgb(78, 78, 78);\n"
 "color: rgb(227, 227, 227); border: 0px;")
         self.delete_fingerprint_button.setObjectName("delete_fingerprint_button")
+        self.delete_fingerprint_button.clicked.connect(lambda: self.changePage(3))
         self.pages = QtWidgets.QStackedWidget(self.centralwidget)
         self.pages.setGeometry(QtCore.QRect(180, 40, 611, 391))
         self.pages.setAutoFillBackground(False)
@@ -74,12 +78,12 @@ class Ui_MainWindow(object):
         self.fp_pushButton_on = QtWidgets.QPushButton(self.fingerprint_page)
         self.fp_pushButton_on.setGeometry(QtCore.QRect(50, 135, 121, 121))
         self.fp_pushButton_on.setStyleSheet("background-color: rgb(60, 60, 60);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.fp_pushButton_on.setObjectName("fp_pushButton_on")
         self.fp_pushButton_out = QtWidgets.QPushButton(self.fingerprint_page)
         self.fp_pushButton_out.setGeometry(QtCore.QRect(175, 135, 121, 121))
         self.fp_pushButton_out.setStyleSheet("background-color: rgb(60, 60, 60);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.fp_pushButton_out.setObjectName("fp_pushButton_out")
         self.pages.addWidget(self.fingerprint_page)
         self.outing_page = QtWidgets.QWidget()
@@ -113,85 +117,85 @@ class Ui_MainWindow(object):
         self.out_pushButton_gohan = QtWidgets.QPushButton(self.outing_page)
         self.out_pushButton_gohan.setGeometry(QtCore.QRect(50, 135, 121, 57))
         self.out_pushButton_gohan.setStyleSheet("background-color: rgb(60, 60, 60);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.out_pushButton_gohan.setObjectName("out_pushButton_gohan")
         self.out_pushButton_lib = QtWidgets.QPushButton(self.outing_page)
         self.out_pushButton_lib.setGeometry(QtCore.QRect(175, 135, 121, 57))
         self.out_pushButton_lib.setStyleSheet("background-color: rgb(60, 60, 60);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.out_pushButton_lib.setObjectName("out_pushButton_lib")
         self.out_pushButton_walk = QtWidgets.QPushButton(self.outing_page)
         self.out_pushButton_walk.setGeometry(QtCore.QRect(50, 200, 121, 57))
         self.out_pushButton_walk.setStyleSheet("background-color: rgb(60, 60, 60);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.out_pushButton_walk.setObjectName("out_pushButton_walk")
         self.out_pushButton_else = QtWidgets.QPushButton(self.outing_page)
         self.out_pushButton_else.setGeometry(QtCore.QRect(175, 200, 121, 57))
         self.out_pushButton_else.setStyleSheet("background-color: rgb(60, 60, 60);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.out_pushButton_else.setObjectName("out_pushButton_else")
         self.pages.addWidget(self.outing_page)
         self.new_fingerprint_page = QtWidgets.QWidget()
         self.new_fingerprint_page.setObjectName("new_fingerprint_page")
         self.new_pushButton_1 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_1.setGeometry(QtCore.QRect(10, 180, 100, 100))
-        self.new_pushButton_1.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_1.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_1.setObjectName("new_pushButton_1")
         self.new_pushButton_2 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_2.setGeometry(QtCore.QRect(110, 180, 100, 100))
-        self.new_pushButton_2.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_2.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_2.setObjectName("new_pushButton_2")
         self.new_pushButton_3 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_3.setGeometry(QtCore.QRect(210, 180, 100, 100))
-        self.new_pushButton_3.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_3.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_3.setObjectName("new_pushButton_3")
         self.new_pushButton_4 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_4.setGeometry(QtCore.QRect(310, 180, 100, 100))
-        self.new_pushButton_4.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_4.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_4.setObjectName("new_pushButton_4")
         self.new_pushButton_5 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_5.setGeometry(QtCore.QRect(410, 180, 100, 100))
-        self.new_pushButton_5.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_5.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_5.setObjectName("new_pushButton_5")
         self.new_pushButton_back = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_back.setGeometry(QtCore.QRect(510, 180, 100, 100))
-        self.new_pushButton_back.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_back.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_back.setObjectName("new_pushButton_back")
         self.new_pushButton_6 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_6.setGeometry(QtCore.QRect(10, 290, 100, 100))
-        self.new_pushButton_6.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_6.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_6.setObjectName("new_pushButton_6")
         self.new_pushButton_7 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_7.setGeometry(QtCore.QRect(110, 290, 100, 100))
-        self.new_pushButton_7.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_7.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_7.setObjectName("new_pushButton_7")
         self.new_pushButton_8 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_8.setGeometry(QtCore.QRect(210, 290, 100, 100))
-        self.new_pushButton_8.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_8.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_8.setObjectName("new_pushButton_8")
         self.new_pushButton_9 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_9.setGeometry(QtCore.QRect(310, 290, 100, 100))
-        self.new_pushButton_9.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_9.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_9.setObjectName("new_pushButton_9")
         self.new_pushButton_0 = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_0.setGeometry(QtCore.QRect(410, 290, 100, 100))
-        self.new_pushButton_0.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_0.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_0.setObjectName("new_pushButton_0")
         self.new_pushButton_ok = QtWidgets.QPushButton(self.new_fingerprint_page)
         self.new_pushButton_ok.setGeometry(QtCore.QRect(510, 290, 100, 100))
-        self.new_pushButton_ok.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.new_pushButton_ok.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.new_pushButton_ok.setObjectName("new_pushButton_ok")
         self.new_label_text = QtWidgets.QLabel(self.new_fingerprint_page)
         self.new_label_text.setGeometry(QtCore.QRect(40, 0, 521, 160))
@@ -207,63 +211,63 @@ class Ui_MainWindow(object):
         self.delete_fingerprint_page.setObjectName("delete_fingerprint_page")
         self.delete_pushButton_1 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_1.setGeometry(QtCore.QRect(10, 180, 100, 100))
-        self.delete_pushButton_1.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_1.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_1.setObjectName("delete_pushButton_1")
         self.delete_pushButton_2 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_2.setGeometry(QtCore.QRect(110, 180, 100, 100))
-        self.delete_pushButton_2.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_2.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_2.setObjectName("delete_pushButton_2")
         self.delete_pushButton_3 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_3.setGeometry(QtCore.QRect(210, 180, 100, 100))
-        self.delete_pushButton_3.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_3.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_3.setObjectName("delete_pushButton_3")
         self.delete_pushButton_4 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_4.setGeometry(QtCore.QRect(310, 180, 100, 100))
-        self.delete_pushButton_4.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_4.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_4.setObjectName("delete_pushButton_4")
         self.delete_pushButton_5 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_5.setGeometry(QtCore.QRect(410, 180, 100, 100))
-        self.delete_pushButton_5.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_5.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_5.setObjectName("delete_pushButton_5")
         self.delete_pushButton_back = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_back.setGeometry(QtCore.QRect(510, 180, 100, 100))
-        self.delete_pushButton_back.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_back.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_back.setObjectName("delete_pushButton_back")
         self.delete_pushButton_6 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_6.setGeometry(QtCore.QRect(10, 290, 100, 100))
-        self.delete_pushButton_6.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_6.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_6.setObjectName("delete_pushButton_6")
         self.delete_pushButton_7 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_7.setGeometry(QtCore.QRect(110, 290, 100, 100))
-        self.delete_pushButton_7.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_7.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_7.setObjectName("delete_pushButton_7")
         self.delete_pushButton_8 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_8.setGeometry(QtCore.QRect(210, 290, 100, 100))
-        self.delete_pushButton_8.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_8.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_8.setObjectName("delete_pushButton_8")
         self.delete_pushButton_9 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_9.setGeometry(QtCore.QRect(310, 290, 100, 100))
-        self.delete_pushButton_9.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_9.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_9.setObjectName("delete_pushButton_9")
         self.delete_pushButton_0 = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_0.setGeometry(QtCore.QRect(410, 290, 100, 100))
-        self.delete_pushButton_0.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_0.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_0.setObjectName("delete_pushButton_0")
         self.delete_pushButton_ok = QtWidgets.QPushButton(self.delete_fingerprint_page)
         self.delete_pushButton_ok.setGeometry(QtCore.QRect(510, 290, 100, 100))
-        self.delete_pushButton_ok.setStyleSheet("background-color: rgb(50, 50, 50);\n"
-"color: rgb(227, 227, 227); border: 0px;")
+        self.delete_pushButton_ok.setStyleSheet("background-color: rgb(60, 60, 60);\n"
+"color: rgb(227, 227, 227); border: 0.5px solid rgb(70, 70, 70);")
         self.delete_pushButton_ok.setObjectName("delete_pushButton_ok")
         self.delete_pushButton_text = QtWidgets.QLabel(self.delete_fingerprint_page)
         self.delete_pushButton_text.setGeometry(QtCore.QRect(40, 0, 521, 160))
@@ -328,8 +332,14 @@ class Ui_MainWindow(object):
         self.delete_pushButton_0.setText(_translate("MainWindow", "0"))
         self.delete_pushButton_ok.setText(_translate("MainWindow", "확인"))
         self.delete_pushButton_text.setText(_translate("MainWindow", "학번을 입력해주세요\n"
-"\n"
-"2423002"))
+"\n" "2423002"))
+            
+    def changePage(self, pageIndex):
+         self.pages.setCurrentIndex(pageIndex)
+         self.buttons = [self.fingerprint_button, self.outing_button, self.new_fingerprint_button, self.delete_fingerprint_button]
+         for button in self.buttons:
+            button.setStyleSheet("background-color: rgb(78, 78, 78); color: rgb(227, 227, 227); border: 0px;")
+            self.buttons[pageIndex].setStyleSheet("background-color: gray; color: rgb(227, 227, 227); border: 0px;")
 
 
 if __name__ == "__main__":
