b5985c974bd4372b76d9bac151f7e5abf1cedf4d
kyumin1227 <kyumin12271227@gmail.com>
2025-05-04 16:43:45 +0900

    Feat: 파이썬 개발용 API 테스터 수정

--- fingerPrint/fingerPrint_kyumin/fingerPrintAPI.py
+++ fingerPrint/fingerPrint_kyumin/fingerPrintAPI.py
@@ -35,75 +35,110 @@ def decrypt(encrypted_data, key):
     decryptor = cipher.decryptor()
     return decryptor.update(encrypted_data) + decryptor.finalize()
 
-# 사용 예
-password = b'my secret password'  # 비밀번호
-salt = urandom(16)  # 솔트 생성
-print(salt)
-key = generate_key(password, salt)  # 키 생성
-
-DUMMY_BYTE_DATA1 = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
-DUMMY_BYTE_DATA2 = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
-SERVER_URL = "http://localhost:8080"
-
-lock = base64.b64encode(encrypt(DUMMY_BYTE_DATA1, key)).decode("utf-8")
-
-print(lock)
-print()
-print()
-print(decrypt(base64.b64decode(lock.encode("utf-8")), key))
-
-
-
+# 상수 정의
+SERVER_URL = "http://210.101.236.158:8081/api"
 headers = {
     'Content-Type': 'application/json'
 }
 
-while True:
-    select1 = int(input("요청할 번호"))
+# 더미 데이터
+DUMMY_BYTE_DATA1 = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
+DUMMY_BYTE_DATA2 = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
 
-    if select1 == 1:
+def check_student(student_number):
+    """가입 가능한 학번인지 체크"""
+    try:
+        res = requests.get(f"{SERVER_URL}/fingerprint/students/{student_number}")
+        print(f"응답: {res.status_code}")
+        print(res.json() if res.status_code == 200 else "응답 없음")
+    except Exception as e:
+        print(f"학번 체크 중 오류 발생: {e}")
 
-        # 가입 가능한 학번인지 체크 요청
-        res = requests.get(f"{SERVER_URL}/fingerprint/students/2423002")
-
-        print(res)
-
-    elif select1 == 2:
-
-        # 학번 및 지문 데이터 전송
+def register_fingerprint(student_number):
+    """학번 및 지문 데이터 등록"""
+    try:
         data = {
             "fingerprint1": base64.b64encode(DUMMY_BYTE_DATA1).decode("utf-8"),
             "fingerprint2": base64.b64encode(DUMMY_BYTE_DATA2).decode("utf-8"),
-            "std_num": "2423003"
+            "std_num": student_number
         }
+        res = requests.post(f"{SERVER_URL}/fingerprint/students", data=json.dumps(data), headers=headers)
+        print(f"응답: {res.status_code}")
+        print(res.json() if res.status_code == 200 else "응답 없음")
+    except Exception as e:
+        print(f"지문 등록 중 오류 발생: {e}")
 
-        res = requests.post(f"{SERVER_URL}/fingerprint/students", data= json.dumps(data), headers=headers)
-
-        print(res)
-
-    elif select1 == 3:
-
-        # 로그 전송
+def send_log(student_number, action):
+    """로그 전송"""
+    try:
         data = {
-            "std_num": "2423002",
-            "action": "등교"
+            "std_num": student_number,
+            "action": action
         }
-
         res = requests.post(f"{SERVER_URL}/fingerprint/logs", data=json.dumps(data), headers=headers)
+        print(f"응답: {res.status_code}")
+        print(res.json() if res.status_code == 200 else "응답 없음")
+    except Exception as e:
+        print(f"로그 전송 중 오류 발생: {e}")
 
-        print(res)
-
-    elif select1 == 4:
-
-        # 재기동 시 지문 데이터 받아오기
+def get_all_fingerprints():
+    """전체 지문 데이터 조회"""
+    try:
         res = requests.get(f"{SERVER_URL}/fingerprint/students")
+        if res.status_code == 200:
+            jsonData = res.json()
+            dataList = jsonData["data"]
+            print(f"총 {len(dataList)}개의 지문 데이터가 있습니다.")
+            for data in dataList:
+                print(f"학번: {data['studentNumber']}")
+                print(base64.b64decode(data["fingerPrintImage1"].encode("utf-8")))
+        else:
+            print("데이터 조회 실패")
+    except Exception as e:
+        print(f"지문 데이터 조회 중 오류 발생: {e}")
 
-        jsonData = res.json()
+def print_menu():
+    """메뉴 출력"""
+    print("\n=== 지문 인식 시스템 API 테스트 ===")
+    print("1. 학번 체크")
+    print("2. 지문 등록")
+    print("3. 로그 전송")
+    print("4. 전체 지문 데이터 조회")
+    print("5. 종료")
 
-        dataList = jsonData["data"]
+def main():
+    while True:
+        print_menu()
+        try:
+            select = int(input("\n요청할 번호를 입력하세요: "))
+            
+            if select == 1:
+                student_number = input("체크할 학번을 입력하세요: ")
+                check_student(student_number)
+                
+            elif select == 2:
+                student_number = input("등록할 학번을 입력하세요: ")
+                register_fingerprint(student_number)
+                
+            elif select == 3:
+                student_number = input("학번을 입력하세요: ")
+                action = input("행동을 입력하세요 (등교/하교/식사/도서관/기타/복귀): ")
+                send_log(student_number, action)
+                
+            elif select == 4:
+                get_all_fingerprints()
+                
+            elif select == 5:
+                print("프로그램을 종료합니다.")
+                break
+                
+            else:
+                print("잘못된 번호를 입력했습니다.")
+                
+        except ValueError:
+            print("올바른 숫자를 입력해주세요.")
+        except Exception as e:
+            print(f"오류 발생: {e}")
 
-        for data in dataList:
-            print(base64.b64decode(data["fingerPrintImage1"].encode("utf-8")))
-
-    elif select1 == 5:
-        break
+if __name__ == "__main__":
+    main()
